52f99370b4af82bc07ef9c600b3f01c1bd86e686
suhyuuun <suhyuuuuuun@gmail.com>
2023-01-25 21:46:50 +0900

    update

--- springboot_01_demo/src/main/java/com/example/demo/MemDTO.java
+++ springboot_01_demo/src/main/java/com/example/demo/MemDTO.java
@@ -1,6 +1,13 @@
 package com.example.demo;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor //parameter가 없는 생성자
+
+@Setter
+@Getter
 public class MemDTO {
 	private String name;
 	private int age;

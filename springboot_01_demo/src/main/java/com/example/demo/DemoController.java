e3960f8c6c52f29f8fe9f00099b3e132e09c6298
suhyuuun <suhyuuuuuun@gmail.com>
2023-01-25 21:02:19 +0900

    update

---
+++ springboot_01_demo/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//http://localhost:8090
+//http://localhost:8090/hello.do
+
+
+@Controller
+public class DemoController {
+
+	@ResponseBody //return값을 json형식으로 넘겨주겠다.
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("Hello Boot~!~");
+		return "Hello Boot!!";
+	}
+	
+	// src\main\webapp\WEB-INF\views 폴더 생성
+	
+	@RequestMapping("/hello.do")
+	public String hello(Model model) {
+		System.out.println("model");
+		model.addAttribute("message", "hello");
+		return "hello";
+	}
+}
---
+++ springboot_01_demo/src/main/java/com/example/demo/Springboot01DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot01DemoApplication.class, args);
+	}
+	//기본적으로 돌아가기 때문에 지우면 안됨
+
+}

a8def3126295680d7a900602e70100cd18486ad6
suhyuuun <suhyuuuuuun@gmail.com>
2023-02-09 15:36:29 +0900

    20230209 | update

--- springboot03_board/src/main/java/com/example/security/service/CorsRefFilter.java
+++ springboot03_board/src/main/java/com/example/security/service/CorsRefFilter.java
@@ -41,6 +41,8 @@ public class CorsRefFilter implements Filter{
 	        response.setHeader("Access-Control-Max-Age", "3600");
 	        response.setHeader("Access-Control-Allow-Headers",
 	                "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+	        
+	        response.setHeader("Access-Control-Expose-Headers", "Authorization");
 
 	        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
 	            response.setStatus(HttpServletResponse.SC_OK);
